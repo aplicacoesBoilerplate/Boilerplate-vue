9c2089d3ca8a5ea3703ed682d0e2b5f98cc3d30d
gersin <gersonfernandesribeiro48@gmail.com>
2025-10-05 11:16:48 -0300

    Ajuste na funcionalidade de logout da sessão ativa do WhatsApp

--- src/components/dialog/dialogQrcode/DialogQrcode.vue
+++ src/components/dialog/dialogQrcode/DialogQrcode.vue
@@ -1,8 +1,17 @@
 <template>
     <BaseDialog v-model:atributos="dialogState">
-        <template #titulo>
-            <v-icon>mdi-cellphone-wireless</v-icon>
-            Conectar ao WhatsApp
+        <template #titulo v-if="!statusConnectModel">
+            <div class="d-flex justify-center">
+                <v-icon class="pr-3">mdi-cellphone-wireless</v-icon>
+                Conectar ao WhatsApp
+            </div>
+        </template>
+
+        <template #titulo v-else>
+            <div class="d-flex justify-center">
+                <v-icon class="pr-3">mdi-cellphone-wireless</v-icon>
+                Desconectar WhatsApp
+            </div>
         </template>
 
         <template #default v-if="!statusConnectModel">
@@ -15,8 +24,12 @@
 
         <template #default v-else>
             <v-list dense>
-                <v-list-item class="d-flex justify-center">
-                    Encerrar sessão?
+                <v-list-item class="d-flex justify-center text-info">
+                    Deseja encerrar a sua sessão?
+                </v-list-item>
+                <!-- Loading -->
+                <v-list-item class="d-flex justify-center" v-if="loading">
+                    <v-progress-circular color="primary" indeterminate />
                 </v-list-item>
             </v-list>
         </template>
@@ -24,8 +37,9 @@
         <template #outrasAcoes v-if="statusConnectModel">
             <v-btn color="yellow-lighten-1" variant="plain" @click="logoutSessioWpp()">
                 <v-icon class="pt-1">mdi-logout</v-icon>
-                Encerrar sessão
+                Encerrar
             </v-btn>
+            <v-spacer />
         </template>
 
     </BaseDialog>
@@ -38,8 +52,11 @@ import BaseDialog from '../BaseDialog.vue';
 // Services
 import { wppConnectionServices } from '@/services/wppConnectionServices';
 
+// Stores
+import { useSnackbarStore } from '@/stores/SnackbarStore';
+
 // Vue
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 const statusConnectModel = defineModel<boolean>('status', { required: true });
 const qrCodeModel = defineModel<{ visualizar: boolean, qrcode: string }>('qrcode', { required: true });
@@ -54,8 +71,23 @@ const dialogState = computed({
     }
 });
 
+const loading = ref(false)
 async function logoutSessioWpp() {
-    await wppConnectionServices.logoutSession();
+    try {
+        loading.value = true
+        await wppConnectionServices.logoutSession();
+        useSnackbarStore().showSnackbar('Sua sessão será encerrada em alguns instantes...!', 'info');
+        setTimeout(() => {
+            qrCodeModel.value.visualizar = false
+            statusConnectModel.value = false
+        }, 3000);
+
+    } catch (error) {
+        useSnackbarStore().showSnackbar(`Erro ao encerrar a sessão! ${error}`, 'red');
+
+    } finally {
+        loading.value = false
+    }
 }
 
 </script>
