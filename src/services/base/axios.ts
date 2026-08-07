7e00c7ad1af1e061f1d6e235afdb5d3ebc72e1d5
gersin <gersonfernandesribeiro48@gmail.com>
2026-08-06 21:15:02 -0300

    fix: configuração do axios corrigida para usar CTradutor e IErros

--- src/services/base/axios.ts
+++ src/services/base/axios.ts
@@ -1,11 +1,11 @@
-// Ecosssitema Vue
 // Axios
 import axios, { AxiosError } from 'axios';
 
-// Types e Interfaces
-import type { IErrorAPI } from '@/models/model/errors/IErrorAPI';
+// Models
+import type { IErros } from '@/models/model/common/IErros';
 
-import { i18n } from '@/plugins/i18n';
+// Classes
+import { CTradutor } from '@/classes/Utils/CTradutor';
 
 const http = axios.create({
   baseURL: window.env?.VITE_API_URL || import.meta.env.VITE_API_URL || 'http://localhost:8080',
@@ -16,41 +16,41 @@ const http = axios.create({
   },
 });
 
-http.interceptors.request.use((config) => {
+http.interceptors.request.use((pConfig) => {
   const token = sessionStorage.getItem('token');
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    pConfig.headers.Authorization = `Bearer ${token}`;
   }
-  // @ts-ignore
-  config.headers['Accept-Language'] = i18n.global.locale.value;
 
-  return config;
+  pConfig.headers['Accept-Language'] = CTradutor.locale;
+
+  return pConfig;
 });
 
 http.interceptors.response.use(
-  (response) => response,
-  (error: AxiosError) => {
-    if (!error) {
+  (pResponse) => pResponse,
+  (pError: AxiosError) => {
+    if (!pError) {
       return Promise.reject(new Error('Erro desconhecido!'));
     }
 
-    const errorResponse = error?.response;
+    const errorResponse = pError?.response;
 
     if (errorResponse?.status === 401) {
       sessionStorage.removeItem('token');
-      void import('@/router').then(({ default: router }) => router.push({ name: 'Login' }));
+      void import('@/router').then(({ default: pRouter }) => pRouter.push({ name: 'Login' }));
       return Promise.reject('Sessão expirada! faça login novamente.');
     }
 
-    if (errorResponse?.status === 403 && error.config?.url !== '/auth/me/cargo') {
-      void import('@/stores/auth.store').then(({ useAuthStore }) =>
-        useAuthStore().atualizarPermissoesUsuarioAutenticado(),
+    if (errorResponse?.status === 403 && pError.config?.url !== '/auth/me/cargo') {
+      void import('@/stores/auth.store').then(({ useAuthStore: pUseAuthStore }) =>
+        pUseAuthStore().atualizarPermissoesUsuarioAutenticado(),
       );
     }
 
-    const objetoError = error.response?.data as IErrorAPI;
+    const objetoError = pError.response?.data as IErros;
 
-    return Promise.reject(objetoError?.mensagem ?? error.message ?? 'messages.errors.reqGenerics');
+    return Promise.reject(objetoError?.mensagem ?? pError.message ?? 'messages.errors.reqGenerics');
   },
 );
 
